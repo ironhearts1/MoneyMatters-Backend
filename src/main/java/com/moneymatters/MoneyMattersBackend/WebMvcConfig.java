725c9904798206e4819443af06f6b3bc1e445a67
ironhearts1 <jmanlambor@gmail.com>
2023-03-21 22:07:21 -0400

    fixing cors issue

--- src/main/java/com/moneymatters/MoneyMattersBackend/WebMvcConfig.java
+++ src/main/java/com/moneymatters/MoneyMattersBackend/WebMvcConfig.java
@@ -1,9 +1,10 @@
 package com.moneymatters.MoneyMattersBackend;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
