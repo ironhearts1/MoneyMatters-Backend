43ff91304252a5b0ab000e41dbf0b1ba5ad875df
ironhearts1 <jmanlambor@gmail.com>
2023-03-21 21:59:01 -0400

    fixing cors issue

--- src/main/java/com/moneymatters/MoneyMattersBackend/WebMvcConfig.java
+++ src/main/java/com/moneymatters/MoneyMattersBackend/WebMvcConfig.java
@@ -8,6 +8,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowCredentials(false).maxAge(-1);
     }
 }
